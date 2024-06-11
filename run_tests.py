e407e3212ecc87d78044753f7ddc3656bfa6fdd1
sharathshari6895 <111352259+sharathshari6895@users.noreply.github.com>
2024-06-11 13:08:11 +0530

    Update run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -3,23 +3,10 @@ import logging
 import sys
 from concurrent.futures import ThreadPoolExecutor
 
-# Set up logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
 def run_tests(test_path):
-    try:
-        logging.info(f"Running tests in {test_path}...")
+    result = subprocess.run([sys.executable, "-m", "pytest", test_path, "--alluredir=./allure_results"], check=True)
 
-        # Use sys.executable to call pytest
-        result = subprocess.run([sys.executable, "-m", "pytest", test_path, "--alluredir=./allure_results"], check=True)
-        logging.info(f"Tests in {test_path} finished with return code: {result.returncode}")
-    except subprocess.CalledProcessError as e:
-        logging.error(f"An error occurred while running tests in {test_path}: {e}")
-    except FileNotFoundError as e:
-        logging.error(f"Executable not found: {e}")
-
-def main():
-    # Define test files
+def main():    
     test_files = [
         "./Tests/UI_Test",
         "./Tests/API_Test",
@@ -36,8 +23,6 @@ def main():
                 future.result(timeout=3600)
             except Exception as e:
                 logging.error(f"An error occurred: {e}")
-
-    logging.info("All test files have been executed.")
 
 if __name__ == "__main__":
     main()
