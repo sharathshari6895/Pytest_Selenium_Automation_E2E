51fb623545ceeb7558cdf3c4a6fff07e411e57d9
sharathshari6895 <111352259+sharathshari6895@users.noreply.github.com>
2024-06-11 12:59:57 +0530

    Update run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -1,37 +1,40 @@
 import subprocess
+import logging
 from concurrent.futures import ThreadPoolExecutor
-from Utils.file_util import ui_test_file, api_test_file, mobile_test_file, accessibility_test_file
 
-"""Run tests in the specified test file using pytest and generate Allure results."""
-"""subprocess module allows you to run external commands, connect to their input/output/error pipes, and obtain their return codes."""
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+def run_tests(test_path):
+    try:
+        logging.info(f"Running tests in {test_path}...")
+        result = subprocess.run(["pytest", test_path, "--alluredir=./allure_results"], check=True)
+        logging.info(f"Tests in {test_path} finished with return code: {result.returncode}")
+    except subprocess.CalledProcessError as e:
+        logging.error(f"An error occurred while running tests in {test_path}: {e}")
+    except FileNotFoundError as e:
+        logging.error(f"Executable not found: {e}")
 
-def run_tests():
-    subprocess.run(["pytest", "--alluredir=./allure_results"])
-
-
-# Create a ThreadPoolExecutor with 3 worker threads
-# with ThreadPoolExecutor(max_workers=4) as executor:
-#     futures = [
-#         # executor.submit(run_tests, ui_test_file),
-#           executor.submit(run_tests, "./Tests/UI_Test"),
-#         executor.submit(run_tests, api_test_file),
-#         executor.submit(run_tests, mobile_test_file),
-#         executor.submit(run_tests, accessibility_test_file)
-#     ]
-
-with ThreadPoolExecutor(max_workers=1) as executor:
-    futures = [
-        executor.submit(run_tests)
-         
+def main():
+    # Define test files
+    test_files = [
+        "./Tests/UI_Test",
+        "./Tests/API_Test",
+        "./Tests/Mobile_Test",
+        "./Tests/Accessibility_Test"
     ]
 
-    # Wait for all tasks to complete, allowing a timeout for acquiring the lock
-    for future in futures:
-        try:
-            future.result(timeout=3600)
-        except Exception as e:
-            print(f"An error occurred: {e}")
+    with ThreadPoolExecutor(max_workers=4) as executor:
+        futures = [executor.submit(run_tests, test_file) for test_file in test_files]
 
-print("All test files have been executed.")
+        # Wait for all tasks to complete
+        for future in futures:
+            try:
+                future.result(timeout=3600)
+            except Exception as e:
+                logging.error(f"An error occurred: {e}")
 
+    logging.info("All test files have been executed.")
+
+if __name__ == "__main__":
+    main()
