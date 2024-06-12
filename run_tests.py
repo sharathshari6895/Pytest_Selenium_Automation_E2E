bced9401cb684fd328e5851f7ce204edf71b56b7
sharathshari6895 <111352259+sharathshari6895@users.noreply.github.com>
2024-06-12 11:06:57 +0530

    Update run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -9,7 +9,7 @@ def run_tests(test_path):
 def main():    
     test_files = [
         # "./Tests/UI_Test",
-        # "./Tests/API_Test",
+        "./Tests/API_Test",
         # "./Tests/Mobile_Test",
         "./Tests/Accessibility_Test"
     ]
